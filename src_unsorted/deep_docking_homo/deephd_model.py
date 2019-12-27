b35dbe586d642d6855f27fd28120db382a9d6a84
Alexander Kalinovsky (lab225ubu) <gakarak@gmail.com>
2019-12-27 07:23:21 +0300

    in progress...

--- src_unsorted/deep_docking_homo/deephd_model.py
+++ src_unsorted/deep_docking_homo/deephd_model.py
@@ -5,13 +5,74 @@ __author__ = 'ar'
 import torch
 from torch import nn
 from torch import functional as F
+from torchvision.models import resnet34
+from typing import Union as U, Optional as O
+
+
+
+class DilatedResNetSEBlock(nn.Module):
+
+    def __init__(self, inpc: int, outc: int, dilation: U[int, tuple, list] = 1):
+        super().__init__()
+        self.inpc = inpc
+        self.outc = outc
+        if isinstance(dilation, int):
+            self.dilation = [dilation]
+        else:
+            self.dilation = list(dilation)
+
+
+
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+
+
+
+class BasicBlock_BAD(nn.Module):
+    expansion = 1
+
     def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=None, dilation=1):
-        super(BasicBlock, self).__init__()
+        super(BasicBlock_BAD, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         self.project_down = conv1x1(128, 64, stride=1)
@@ -159,5 +220,13 @@ def prospr(**kwargs):
     return model
 
 
+def main_debug():
+    from torchvision.models import resnet18, resnet34, resnet50
+
+    print('-')
+
+
 if __name__ == '__main__':
-    pass
+    main_debug()
+
+
