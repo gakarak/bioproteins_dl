19fad57e2de55c4f9a54c15433669ec53fd76ef5
Alexander Kalinovsky (lab225ubu) <gakarak@gmail.com>
2019-12-30 01:39:38 +0300

    1'st version of Deep Residual ASPP with SE block network for homo-docking

--- src_unsorted/deep_docking_homo/deephd_model.py
+++ src_unsorted/deep_docking_homo/deephd_model.py
@@ -2,134 +2,187 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+import numpy as np
 import torch
 from torch import nn
 from torch import functional as F
 from torchvision.models import resnet34
 from typing import Union as U, Optional as O
+from segmentation_models_pytorch import Unet, FPN, Linknet, PSPNet
 
 
+def split_int_to_chunks(val: int, num: int) -> tuple:
+    s = val // num
+    if s * num < val:
+        ret = [s] * (val // s)
+        ret += [val - s * num]
+    else:
+        ret = [s] * num
+    return tuple(ret)
 
-class DilatedResNetSEBlock(nn.Module):
 
-    def __init__(self, inpc: int, outc: int, dilation: U[int, tuple, list] = 1):
+def get_nlin(nlin: str):
+    if nlin == 'relu':
+        ret = nn.ReLU(inplace=True)
+    elif nlin == 'elu':
+        ret = nn.ELU(inplace=True)
+    elif nlin == 'prelu':
+        ret = nn.PReLU()
+    elif nlin == 'lrelu':
+        ret = nn.LeakyReLU(inplace=True)
+    else:
+        raise NotImplementedError
+    return ret
+
+
+class ConvBN(nn.Sequential):
+
+    def __init__(self, inp: int, out: int, ks: int = 3, nlin: O[str] = 'relu', bias=False):
         super().__init__()
-        self.inpc = inpc
-        self.outc = outc
-        if isinstance(dilation, int):
-            self.dilation = [dilation]
-        else:
-            self.dilation = list(dilation)
+        pad = ks // 2
+        body = [
+            nn.Conv2d(in_channels=inp, out_channels=out, kernel_size=ks, bias=bias, padding=pad),
+            nn.BatchNorm2d(out),
+        ]
+        if nlin is not None:
+            body.append(get_nlin(nlin))
+        super().__init__(*body)
 
 
+class ASPPConv(nn.Sequential):
+
+    def __init__(self, in_channels, out_channels, dilation, ks=3, nlin: str = 'relu'):
+        pad = (ks + (ks-1)*(dilation-1) - 1) // 2
+        modules = [
+            nn.Conv2d(in_channels, out_channels, ks, padding=pad, dilation=dilation, bias=False),
+            nn.BatchNorm2d(out_channels),
+            get_nlin(nlin)
+        ]
+        super(ASPPConv, self).__init__(*modules)
 
 
+class ASPPMultiConvBlock(nn.Module):
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
+    def __init__(self, inp_dim: int, out_dim: int, ks=3, ks_prj=1, dilation=(2, ), num_conv=1, nlin: str = 'relu'):
         super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
+        self.inp = inp_dim
+        self.out = out_dim
+        num_aspp = len(dilation)
+        self.proj_inp = ConvBN(inp_dim, out_dim, ks=ks_prj, nlin=nlin)
+        self.proj_out = nn.ModuleList([ConvBN(num_aspp * out_dim, out_dim, ks=ks_prj, nlin=nlin) for x in range(num_conv)])
+        self.aspp = nn.ModuleList()
+        for ci in range(num_conv):
+            aspp = nn.ModuleList([ASPPConv(out_dim, out_dim, x, ks=ks, nlin=nlin) for x in dilation])
+            self.aspp.append(aspp)
 
     def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
+        x = self.proj_inp(x)
+        for aspp_conv, prj in zip(self.aspp, self.proj_out):
+            x = torch.cat([m(x) for m in aspp_conv], dim=1)
+            x = prj(x)
+        return x
 
 
+class CSEBlock(nn.Module):
+
+    def __init__(self, ch, re=16, nlin: str = 'relu'):
+        super().__init__()
+        self.cSE = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                 nn.Conv2d(ch, ch // re, 1),
+                                 get_nlin(nlin),
+                                 nn.Conv2d(ch // re, ch, 1),
+                                 nn.Sigmoid())
+    def forward(self, x):
+        return x * self.cSE(x)
 
 
-
-class BasicBlock_BAD(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=None, dilation=1):
-        super(BasicBlock_BAD, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self.project_down = conv1x1(128, 64, stride=1)
-        self.project_up = conv1x1(64, 128, stride=1)
-        self.bn64_1 = norm_layer(64)
-        self.bn64_2 = norm_layer(64)
-        self.bn128 = norm_layer(128)
-        # dilations deal now with 64 incoming and 64 outcoming layers
-        self.dilation = conv3x3(64, 64, stride, dilation=dilation)  # when the block is initialized, the only thing that changes is the dilation filter used!
-        self.elu = nn.ELU(inplace=True)
+class SCSEBlock(nn.Module):
+    def __init__(self, ch, re=16, nlin: str = 'relu'):
+        super().__init__()
+        self.cSE = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                 nn.Conv2d(ch, ch//re, 1),
+                                 get_nlin(nlin),
+                                 nn.Conv2d(ch//re, ch, 1),
+                                 nn.Sigmoid())
+        self.sSE = nn.Sequential(nn.Conv2d(ch, ch, 1), nn.Sigmoid())
 
     def forward(self, x):
-        identity = x
-        # the deepmind basic block goes:
-        # batchnorm
-        out = self.bn128(x)
-        # elu
-        out = self.elu(out)
-        # project down to 64
-        out = self.project_down(out)
-        # batchnorm
-        out = self.bn64_1(out)
-        # elu
-        out = self.elu(out)
-        # cycle through 4 dilations
-        out = self.dilation(out)
-        # batchnorm
-        out = self.bn64_2(out)
-        # elu
-        out = self.elu(out)
-        # project up to 128
-        out = self.project_up(out)
-        # identitiy addition
-        out = out + identity
-        return out
+        return x * self.cSE(x) + x * self.sSE(x)
 
 
-class ResNet(nn.Module):
+def get_attention(ch: int, atype: str = 'cse'):
+    if atype == 'cse':
+        return CSEBlock(ch=ch)
+    elif atype == 'scse':
+        return SCSEBlock(ch=ch)
+    elif atype is None:
+        return nn.Identity()
+    else:
+        raise NotImplementedError
 
-    def __init__(self, block, layers, num_classes=64 * 64, zero_init_residual=False, norm_layer=None):
-        super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self.inplanes = RESNET_DIM
 
-        self.conv1 = conv1x1(INPUT_DIM, RESNET_DIM, stride=1)
-        self.conv2 = conv1x1(RESNET_DIM, OUTPUT_BINS)
+class BottleneckASPPSEBlock(nn.Module):
 
-        self.proj_aux = conv1x1(RESNET_DIM, 83, stride=1)
-        self.conv_aux = conv64x1(83, 83, stride=1, groups=1)
+    def __init__(self, inp_dim: int, emb_dim: int,
+                 dilation: U[tuple, list] = (2, ), num_conv=1,
+                 ks: int = 3, nlin: str = 'relu', attention_type=None):
+        super().__init__()
+        self.conv_prj_inp = ConvBN(inp_dim, emb_dim, ks=1, nlin=nlin)
+        # self.conv_spatial = ConvBN(emb_dim, emb_dim, ks=ks, nlin=nlin)
+        self.conv_spatial = ASPPMultiConvBlock(emb_dim, emb_dim, ks=ks, nlin=nlin,
+                                               num_conv=num_conv, dilation=dilation)
+        self.conv_prj_out = ConvBN(emb_dim, inp_dim, ks=1, nlin=None)
+        self.attention = get_attention(inp_dim, attention_type)
+        self.out_activation = get_nlin(nlin)
 
-        self.elu = nn.ELU(inplace=True)
+    def forward(self, x):
+        inp = x
+        x = self.conv_prj_inp(x)
+        x = self.conv_spatial(x)
+        x = self.conv_prj_out(x)
+        x = self.attention(x)
+        x += inp
+        x = self.out_activation(x)
+        return x
 
-        self.bn1 = norm_layer(INPUT_DIM)
 
-        self.resnet_blocks = self._make_layer(block, RESNET_DIM, layers[0], norm_layer=norm_layer)
+class MultiBottleneckSEBlock(nn.Module):
 
+    def __init__(self, inp_dim: int, emb_dim: int,
+                 dilation: U[tuple, list] = (2, ), num_conv=1,
+                 ks: int = 3, nlin: str = 'relu', attention=None,
+                 num_blocks=1):
+        super().__init__()
+        inps = [inp_dim] * num_blocks
+        embs = [emb_dim] * num_blocks
+        # dims = [inp_dim] + [emb_dim] * num_blocks
+        body = [BottleneckASPPSEBlock(num_inp, num_emb, dilation=dilation, num_conv=num_conv,
+                                      ks=ks, nlin=nlin, attention_type=attention)
+                for num_inp, num_emb in zip(inps, embs)]
+        self.body = nn.Sequential(*body)
+
+    def forward(self, x):
+        return self.body(x)
+
+
+class ASPPResNet(nn.Module):
+
+    def __init__(self, inp: int, out: int, nlin: str = 'elu', attention='cse'):
+        super().__init__()
+        self.stage1_conv = ASPPMultiConvBlock(inp, 64, dilation=(2, 4, 8), ks_prj=3, nlin=nlin, num_conv=2)
+        self.stage1_res = MultiBottleneckSEBlock(64, 32, dilation=(1, 2, 4), nlin=nlin, num_blocks=2, attention=attention)
+        #
+        self.stage2_conv = ASPPMultiConvBlock(64, 96, dilation=(4, 8, 16), ks_prj=3, nlin=nlin, num_conv=2)
+        self.stage2_res = MultiBottleneckSEBlock(96, 64, dilation=(1, 2, 4), nlin=nlin, num_blocks=3, attention=attention)
+        #
+        self.stage3_conv = ASPPMultiConvBlock(96, 96, dilation=(8, 16, 32), ks_prj=3, nlin=nlin, num_conv=2)
+        self.stage3_res = MultiBottleneckSEBlock(96, 64, dilation=(1, 2, 4), nlin=nlin, num_blocks=3, attention=attention)
+        #
+        self.stage3_conv = ASPPMultiConvBlock(96, 104, dilation=(8, 16, 32), ks_prj=3, nlin=nlin, num_conv=2)
+        self.stage3_res = MultiBottleneckSEBlock(104, 64, dilation=(1, 2, 4), nlin=nlin, num_blocks=3, attention=attention)
+        #
+        self.out_block = nn.Conv2d(104, out, kernel_size=1)
+        #
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
@@ -137,96 +190,57 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, norm_layer=None):
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, norm_layer))
-        self.inplanes = planes * block.expansion
-
-        # here I need to pass in the correct dilations 1,2,4,8
-        dilations = [1, 2, 4, 8]
-
-        for i, _ in enumerate(range(1, blocks)):
-            layers.append(block(self.inplanes, planes, norm_layer=norm_layer, dilation=dilations[i % 4]))
-
-        return nn.Sequential(*layers)
-
     def forward(self, x):
-        # fix input dimensions
-        x = self.bn1(x)  # Why?
-        x = self.conv1(x)
-
-        # propagate through RESNET blocks
-        resnet_out = self.resnet_blocks(x)
-        # renet_out has shape 1,128,64,64
-
-        # fix output dimensions
-        x = self.conv2(resnet_out)  # return 64x64x64
-
-        aux = self.proj_aux(resnet_out)
-        # should we have elu / batchnorm(s) here??
-        aux_i = self.conv_aux(torch.transpose(aux, 2, 3))
-        aux_j = self.conv_aux(aux)
-
-        # FIX THIS TO WORK WITH BATCHES!
-        return x, aux_i[:, :9], aux_j[:, :9], aux_i[:, 9:9 + 37], aux_j[:, 9:9 + 37], aux_i[:,
-                                                                                      9 + 37:9 + 2 * 37], aux_j[:,
-                                                                                                          9 + 37:9 + 2 * 37]
+        x = self.stage1_conv(x)
+        x = self.stage1_res(x)
+        x = self.stage2_conv(x)
+        x = self.stage2_res(x)
+        x = self.stage3_conv(x)
+        x = self.stage3_res(x)
+        x = self.out_block(x)
+        return x
 
 
-def is_training():
-    pass  # change BATCH_SIZE
-
-
-def conv3x3(in_planes, out_planes, stride=1, dilation=1):
-    """3x3 convolution with padding"""
-    padding = 1 + (dilation - 1)  # derived to ensure consistent size
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=padding, bias=True, dilation=dilation)
-
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=True)
-
-
-def conv64x1(in_planes, out_planes, stride=1, groups=1):
-    """64x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=(64, 1), stride=stride, groups=groups, bias=True)
-
-
-# Define the ProSPr CNN:
-
-def prospr(**kwargs):
-    model = ResNet(BasicBlock, [NUM_RESNET_BLOCKS, 0, 0, 0], **kwargs)
-    return model
+def count_model_parameters(model:nn.Module):
+    params_total = np.sum([p.numel() for p in model.parameters()])
+    params_grad = np.sum([p.numel() for p in model.parameters() if p.requires_grad])
+    return {
+        'params_total': params_total,
+        'params_grad': params_grad
+    }
 
 
 def main_debug():
+    import matplotlib.pyplot as plt
     from torchvision.models import resnet18, resnet34, resnet50
-
+    # m = ASPPMultiConvBlock(inp_dim=3, out_dim=16, dilation=(2, 4, 16, 32), num_conv=3, ks=5, ks_prj=3, nlin='elu')
+    # m = Unet(encoder_weights=None, encoder_name='vgg19')
+    # m = Unet(encoder_weights=None, encoder_name='senet154')#, attention_type='scse')
+    m = ASPPResNet(3, 1)
+    print(m)
+    # m = resnet50(num_classes=1)
+    print(count_model_parameters(m))
+    x = torch.zeros([4, 3, 64, 64])
+    x[:, :, x.shape[2] // 2, x.shape[3] // 2] = 1
+    # m = nn.Sequential(*[torch.nn.Conv2d(1, 1, 3, dilation=x, padding=x) for x in [4, 8, 16]])
+    # for z in m.modules():
+    #     if isinstance(z, nn.Conv2d):
+    #         z.weight.data.fill_(1)
+    #         if z.bias is not None:
+    #             z.bias.data.fill_(0)
+    #
+    m.eval()
+    with torch.no_grad():
+        x_ = x.detach().cpu().numpy()
+        y_ = m(x).detach().cpu().numpy()
+        plt.subplot(1, 2, 1), plt.imshow(x_[0, 0])
+        plt.subplot(1, 2, 2), plt.imshow(y_[0, 0])
+        plt.show()
     print('-')
 
 
 if __name__ == '__main__':
     main_debug()
+
 
 
