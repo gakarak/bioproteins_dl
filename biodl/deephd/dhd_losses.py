f2a3fe3fc72c3f5f4de31e9f12a90acfcf8c17dc
Alexander Kalinovsky (deepw) <gakarak@gmail.com>
2020-03-22 10:11:31 +0300

    update losses: add log-version for Jaccard/Dice losses

--- biodl/deephd/dhd_losses.py
+++ biodl/deephd/dhd_losses.py
@@ -7,9 +7,11 @@ import torch
 from torch import nn
 from pytorch_toolbelt.losses import JointLoss
 from segmentation_models_pytorch.utils.losses import DiceLoss, JaccardLoss#, BCEDiceLoss, BCEJaccardLoss
+from segmentation_models_pytorch.utils.losses import Activation, F as F2
+from segmentation_models_pytorch.utils import base
 
 
-def build_loss_by_name(loss_name: str, class_weight = 50.0) -> nn.Module:
+def build_loss_by_name(loss_name: str, class_weight = 30.0) -> nn.Module:
     if loss_name == 'bce':
         # return nn.BCEWithLogitsLoss()
         return BCEWithLogitsLossW(class_weight=class_weight)
@@ -17,17 +19,25 @@ def build_loss_by_name(loss_name: str, class_weight = 50.0) -> nn.Module:
         return nn.L1Loss()
     elif loss_name == 'l2':
         return nn.MSELoss()
+    
     elif loss_name == 'bcedice':
-        # return BCEDiceLoss()
-        # return JointLoss(first=nn.BCEWithLogitsLoss(pos_weight=torch.FloatTensor([100])), second=DiceLoss(activation='sigmoid'))
         return JointLoss(first=BCEWithLogitsLossW(class_weight=class_weight), second=DiceLoss(activation='sigmoid'))
-    elif loss_name == 'bcejaccard':
-        # return BCEJaccardLoss()
+    elif loss_name == 'bceiou':
         return JointLoss(first=BCEWithLogitsLossW(class_weight=class_weight), second=JaccardLoss(activation='sigmoid'))
+    
+    elif loss_name == 'bceldice':
+        return JointLoss(first=BCEWithLogitsLossW(class_weight=class_weight), second=LogDiceLoss(activation='sigmoid'))
+    elif loss_name == 'bceliou':
+        return JointLoss(first=BCEWithLogitsLossW(class_weight=class_weight), second=LogJaccardLoss(activation='sigmoid'))
+    
     elif loss_name == 'dice':
         return DiceLoss(activation='sigmoid')
-    elif loss_name == 'jaccard':
+    elif loss_name == 'iou':
         return JaccardLoss(activation='sigmoid')
+    elif loss_name == 'ldice':
+        return LogDiceLoss(activation='sigmoid')
+    elif loss_name == 'liou':
+        return LogJaccardLoss(activation='sigmoid')
     else:
         raise NotImplementedError
 
@@ -43,6 +53,48 @@ class BCEWithLogitsLossW(nn.Module):
         msk_ = (y_gt > 0) * (self.class_weight - 1) + 1
         loss_ = self.bce(y_pr, y_gt) * msk_
         return loss_.mean()
+
+class LogDiceLoss(base.Loss):
+
+    def __init__(self, eps=1., beta=1., activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.beta = beta
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        loss_ = F2.f_score(
+            y_pr, y_gt,
+            beta=self.beta,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
+        return -torch.log(loss_.clamp_min(1e-6))
+
+
+class LogJaccardLoss(base.Loss):
+
+    def __init__(self, eps=1., beta=1., activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.beta = beta
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        loss_ = F2.jaccard(
+            y_pr, y_gt,
+            beta=self.beta,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
+        return -torch.log(loss_.clamp_min(1e-6))
+
 
 
 def main_debug():
